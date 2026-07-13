c14beba33bc1a05eb47a28d6d374d7504c88af6d
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2026-07-13 16:20:55 +0200

    arch/sh/mm/cache: (Probably unneeded) Gate SH2-derived CCR register location
    Signed-off-by: cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>

--- arch/sh/include/cpu-sh2/cpu/cache.h
+++ arch/sh/include/cpu-sh2/cpu/cache.h
@@ -15,7 +15,10 @@
 #define SH_CACHE_ASSOC		8
 
 #if defined(CONFIG_CPU_SUBTYPE_SH7619)
+
+#if defined(CONFIG_CPU_SUBTYPE_SH7619)
 #define SH_CCR		0xffffffec
+#endif
 
 #define CCR_CACHE_CE	0x01	/* Cache enable */
 #define CCR_CACHE_WT	0x02    /* CCR[bit1=1,bit2=1] */
