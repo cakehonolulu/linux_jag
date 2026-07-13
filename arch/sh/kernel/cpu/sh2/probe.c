d2c37384ee0cea23e8f4d83bfbb73e08a6d3d667
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2026-07-13 16:26:01 +0200

    arch/sh/kernel: Add cache information for SH7604
    Signed-off-by: cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>

--- arch/sh/kernel/cpu/sh2/probe.c
+++ arch/sh/kernel/cpu/sh2/probe.c
@@ -39,6 +39,16 @@ void __ref cpu_probe(void)
 	boot_cpu_data.dcache.flags		= 0;
 #endif
 
+#if defined(CONFIG_CPU_SUBTYPE_SH7604)
+	boot_cpu_data.type			= CPU_SH7604;
+	boot_cpu_data.dcache.ways		= 4;
+	boot_cpu_data.dcache.way_incr	= (1<<12);
+	boot_cpu_data.dcache.sets		= 256;
+	boot_cpu_data.dcache.entry_shift	= 4;
+	boot_cpu_data.dcache.linesz		= L1_CACHE_BYTES;
+	boot_cpu_data.dcache.flags		= 0;
+#endif
+
 #if defined(CONFIG_CPU_J2)
 #if defined(CONFIG_SMP)
 	unsigned cpu = hard_smp_processor_id();
